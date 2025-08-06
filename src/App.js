9d85aa2c5a616265617c4980cf941971ed070a0b
marehanhamed <marehan514@gmail.com>
2025-08-06 14:31:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import Navbars from './components/Nav/Navbar';
-import Home from './components/Home/Home';
-import FAQ from './components/Pages/FAQ’S/FAQ’S';
-import LatestNews from './components/LatestNews';
+import AppointmentForm from './components/AppointmentForm'
 
 function App() {
   return (
     <>
-    <Navbars/>
-    <Home/>
-    <FAQ/>
-    <LatestNews/>
+    <AppointmentForm/>
     </>
   );
 }
